54406b547b3da3feea930c1c5adc421c41eeb18d
Betsy <116031639+betsycotter@users.noreply.github.com>
2022-12-07 17:52:11 -0700

    Updated final queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -37,7 +37,33 @@ e.birth_date,
 de.from_date, 
 de.to_date, 
 t.title
-INTO mentorship_eligibilty
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de 
+ON de.emp_no = e.emp_no
+INNER JOIN titles as t 
+ON t.emp_no = e.emp_no
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+-- Additional queries
+-- Retiring employees with department information 
+SELECT DISTINCT ON (ut.emp_no) ut.emp_no, ut.first_name, ut.last_name, ut.title, de.dept_no, d.dept_name
+FROM unique_titles as ut
+INNER JOIN dept_emp as de
+ON de.emp_no = ut.emp_no
+INNER JOIN departments as d 
+ON d.dept_no = de.dept_no;
+
+-- Mentorship Eligibility with department information 
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+e.first_name, 
+e.last_name, 
+e.birth_date, 
+de.from_date, 
+de.to_date, 
+de.dept_no, 
+t.title 
 FROM employees as e
 INNER JOIN dept_emp as de 
 ON de.emp_no = e.emp_no
